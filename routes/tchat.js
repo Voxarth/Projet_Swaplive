e75582da071a987cbc31f0517bd023346d63a48c
Your Name <you@example.com>
2019-04-10 14:50:16 +0000

    commit de rachida

--- routes/tchat.js
+++ routes/tchat.js
@@ -18,5 +18,17 @@ router.delete('/:id', function(req, res, next) {
     // Puis dire à dire à l'utilisateur qu'il à bien supprimer son compte
       res.send('Votre message à bien été supprimer');
     });
+    
+router.post('/:id', function(req, res, next) {
+    res.send('vous avez été ajouté');
+});
 
+
+router.put('/:id', function(req, res, next) {
+    res.send('vous avez été banni de la discution');
+
+    if(!req.body.tchat)
+        res.write('');
+});
+    
 module.exports = app;
