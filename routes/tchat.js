c5b43814c3e030ae35e5fb279b35692843ceb531
zeduvo <flipo.morgann@gmail.com>
2019-04-11 09:26:44 +0200

    modif du tchat.js

--- routes/tchat.js
+++ routes/tchat.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+/**@author Morgann */
 router.post('/', function(req, res, next){
     //Ajout d'une discution
     //Ajout dans la BDD
@@ -8,25 +9,26 @@ router.post('/', function(req, res, next){
     res.send("Votre discution viens d'être créer");
 })
 
+/**@author Morgann */
 router.get('/:id', function(req, res, next){
     //Voir les paramètres d'une discution
     res.send('Voici les paramètres de la discution');
 })
 
-
+/**@author Morgann */
 router.delete('/:id', function(req, res, next) {
     // Récupérer req.params.id
     // L'utiliser pour supprimé en BDD
     // Puis dire à l'utilisateur qu'il a bien supprimé la discution
       res.send('Votre discution à bien été supprimer');
-    });
+});
 
 /** /
 * @author  Rachida
 Ajout d'une discussion
 **/
 router.post('/:id', function(req, res, next) {
-    res.send('vous avez été ajouté');
+    res.send('Vous avez été ajouter');
 });
 
 /** /
@@ -34,7 +36,7 @@ router.post('/:id', function(req, res, next) {
 Supprimer un utilisateur d'une discussion
 **/
 router.put('/:id', function(req, res, next) {
-    res.send('vous avez été banni de la discution');
+    res.send('Vous avez été banni de la discution');
 
     if(!req.body.tchat)
         res.write('');
