e87d6586d73605ede4864d1414ffd2ca45d05d8c
zeduvo <flipo.morgann@gmail.com>
2019-04-11 09:50:19 +0200

    modif tchat.js

--- routes/tchat.js
+++ routes/tchat.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/**@author Morgann */
+/** /
+ * @author Morgann 
+ **/
 router.post('/', function(req, res, next){
     //Ajout d'une discution
     //Ajout dans la BDD
@@ -9,19 +11,35 @@ router.post('/', function(req, res, next){
     res.send("Votre discution viens d'être créer");
 })
 
-/**@author Morgann */
+/** /
+ * @author Morgann 
+ * 
+ **/
 router.get('/:id', function(req, res, next){
     //Voir les paramètres d'une discution
     res.send('Voici les paramètres de la discution');
 })
 
-/**@author Morgann */
+/** /
+ * @author Morgann 
+ * 
+ **/
 router.delete('/:id', function(req, res, next) {
     // Récupérer req.params.id
     // L'utiliser pour supprimé en BDD
     // Puis dire à l'utilisateur qu'il a bien supprimé la discution
       res.send('Votre discution à bien été supprimer');
 });
+
+/** 
+
+@author G_G (Jérém)
+
+**/
+router.get('/:id', function(req, res, next){
+    //Afficher une conversation de la BDD
+    res.send('Voici la conversation');
+})
 
 /** /
 * @author  Rachida
