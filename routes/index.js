6d40dc41899ee8126404aedc384ca17ea93219cf
Your Name <you@example.com>
2019-04-11 11:46:48 +0000

    ajout de mongoDB sur index et tchat

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient=require('mongodb').MongoClient,
+  ObjectId =require('mongodb').ObjectId,
+ url= "mongodb://localhost:27017/swaplive";
+
+ MongoClient.connect(url,
+    {useNewUrlParser:true},
+    function(err,client){
+        if(err) throw err;
+        var DB = client.db('swaplive');
 
 /* GET accueil */
 router.get('/', function(req, res, next) {
@@ -13,6 +22,7 @@ router.post('/login', function(req, res, next) {
 /* GET  CGU. */
 router.get('/cgu', function(req, res, next) {
   res.render('cgu', { title: 'Express' });
+});
 });
 
 module.exports = router;
