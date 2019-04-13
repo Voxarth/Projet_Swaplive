25cb8bd7c54c170fd9d56acc18b4490c396a4d72
Jérémy Doré <j.dore@it-students.fr>
2019-04-13 15:30:55 +0200

    Modif de la partie modification et suppression d'un utilisateur

--- routes/users.js
+++ routes/users.js
@@ -4,77 +4,94 @@ var router = express.Router();
  * @author Voxarth
  * ajout de MongoDB
  */
-var MongoClient=require('mongodb').MongoClient,
- url= "mongodb://localhost:27017/swaplive";
+var MongoClient = require('mongodb').MongoClient,
+  url = "mongodb://localhost:27017/swaplive";
 
- MongoClient.connect(url,
-    {useNewUrlParser:true},
-    function(err,client){
-        if(err) throw err;
-        var DB = client.db('swaplive');
-        console.log('Je suis connecté.');
-
-
-  /* GET users listing. */
-  router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
-  });
+MongoClient.connect(url,
+  { useNewUrlParser: true },
+  function (err, client) {
+    if (err) throw err;
+    var DB = client.db('swaplive');
+    console.log('Je suis connecté.');
 
 
-  /**Création d'un nouveau compte
-   **@author romain
-  */
-  router.post('/', function(req, res, next) {
-    //console.log(req.body);
-    
-    //verifier les données en post
-    if (!req.body.name){
-      return res.send('merci de renseigner votre nom');
-    }
-    if (!req.body.lastName){
-      return res.send('merci de renseigner votre prénom');
-    }
-    if  (!req.body.email){
-        return res.send('merci de renseigner votre email');
-    }
-    if  (!req.body.password){
-      return res.send('merci de renseigner votre mot de passe');
-  }
-
-    //inserer les données dans la BDD
-    DB.collection('users').insertOne(req.body, function(err, result){
-      if(err) throw err; 
-      console.log(result) ;
-      res.json ({
-        result : 'ok',
-        id : result.insertedId.toString()
-      });
+    /* GET users listing. */
+    router.get('/', function (req, res, next) {
+      res.send('respond with a resource');
     });
 
-  });
 
-  /**
-  @author G_G (Jérém)
-  **/
-  // POUR MODIFIER UN UTILISATEUR
-  router.put('/:id', function(req, res, next) {
-    // Récupérer req.params.id
-  // L'utiliser pour modifier en BDD le compte
-  // Puis dire à dire à l'utilisateur qu'il à bien modifier son compte
+    /**Création d'un nouveau compte
+     **@author romain
+    */
+    router.post('/', function (req, res, next) {
+      //console.log(req.body);
+
+      //verifier les données en post
+      if (!req.body.name) {
+        return res.send('merci de renseigner votre nom');
+      }
+      if (!req.body.lastName) {
+        return res.send('merci de renseigner votre prénom');
+      }
+      if (!req.body.email) {
+        return res.send('merci de renseigner votre email');
+      }
+      if (!req.body.password) {
+        return res.send('merci de renseigner votre mot de passe');
+      }
+
+      //inserer les données dans la BDD
+      DB.collection('users').insertOne(req.body, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.json({
+          result: 'ok',
+          id: result.insertedId.toString()
+        });
+      });
+
+    });
+
+    /**
+    @author G_G (Jérém)
+    **/
+    // POUR MODIFIER UN UTILISATEUR
+    router.put('/:id', function (req, res, next) {
+      // Récupérer req.params.id
+      // L'utiliser pour modifier en BDD le compte
+      // Puis dire à dire à l'utilisateur qu'il à bien modifier son compte
+      DB.collection('users').updateOne(req.body, function (err, result) {
+        if (err) throw err;
+        // console.log(result);
+        res.json({
+          result: 'Compte bien modifier',
+          id: result.insertedId.toString()
+        });
+      });
       res.send('Votre compte à été modifier');
     });
 
-  /**
-  @author G_G (Jérém)
-  **/
-  // POUR SUPPRIMER UN UTILISATEUR
-  router.delete('/:id', function(req, res, next) {
-  // Récupérer req.params.id
-  // L'utiliser pour supprimé en BDD le compte
-  // Puis dire à dire à l'utilisateur qu'il à bien supprimer son compte
-    res.send('Votre compte à été supprimer');
+    /**
+    @author G_G (Jérém)
+    **/
+    // POUR SUPPRIMER UN UTILISATEUR
+    router.delete('/id', function (req, res, next) {
+      // Récupérer req.params.id
+      // L'utiliser pour supprimé en BDD le compte
+      // Puis dire à dire à l'utilisateur qu'il à bien supprimer son compte
+      DB.collection('users').deleteOne(req.body, function (err, result) {
+        if (err) throw err;
+        // console.log(result);
+        res.json({
+          result: 'Compte bien supprimer',
+          id: result.insertedId.toString()
+        });
+      });
+
+      res.send('Votre compte à été supprimer');
+    });
   });
-});
 
 
 module.exports = router;
