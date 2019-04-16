123e7f7501402c0bb6f42e774c26ac8f0017f4c6
Jérémy Doré <j.dore@it-students.fr>
2019-04-16 15:22:08 +0200

    Ajouts de tout !

--- routes/index.js
+++ routes/index.js
@@ -24,37 +24,47 @@ MongoClient.connect(url,
     router.get('/createCompte', function (req, res, next) {
       res.render('createCompte', { title: 'SWAP-LIVE' });
     });
-    
-    
+
+    // Page de création de compte
+    router.get('/tchat', function (req, res, next) {
+      res.render('tchat', { title: 'SWAP-LIVE' });
+    });
+
+    // Page de création de compte
+    router.get('/conversations', function (req, res, next) {
+      res.render('conversations', { title: 'SWAP-LIVE' });
+    });
+
+
     /*****se loguer au compte (je l'ai  fait à ma sauce)
        **@author romain */
 
     router.post('/login', function (req, res, next) {
-     //vérifier les données reçu en POST.
-    var requiredProps = ['password', 'email'];
-    for (var i in requiredProps) {
-      if(typeof req.body[requiredProps[i]] == 'undefined'){
-        console.log(requiredProps [i] + 'empty');
-        return res.send(requiredProps[i] + 'empty');
+      //vérifier les données reçu en POST.
+      var requiredProps = ['password', 'email'];
+      for (var i in requiredProps) {
+        if (typeof req.body[requiredProps[i]] == 'undefined') {
+          console.log(requiredProps[i] + 'empty');
+          return res.send(requiredProps[i] + 'empty');
+        }
       }
-    }
       //inserer les données reçu du logg dans la BDD.
-      DB.collection('users').findOne({email :req.body.email}, function (err, result){
+      DB.collection('users').findOne({ email: req.body.email }, function (err, result) {
         if (err) throw err;
         console.log(result);
         //réponse au client avec $id du compte.
-        if (result == '' || result ==null){
+        if (result == '' || result == null) {
           return res.send('Email non valide');
         }
-        if (result.password != req.body.password || req.body.password == null){
-          return res.send('Password error') ;
+        if (result.password != req.body.password || req.body.password == null) {
+          return res.send('Password error');
         }
-        else{
-          
-          res.cookie('token', result._id.toString()) ;
-          connectedUsers.set(result._id.toString(), result) ;
+        else {
+
+          res.cookie('token', result._id.toString());
+          connectedUsers.set(result._id.toString(), result);
           res.json({
-            result : 'connexion réussis YALLLLA',
+            result: 'connexion réussis YALLLLA',
             // id : result._id,
             // name : result.name,
             // lastName : result.lastName,
@@ -62,16 +72,16 @@ MongoClient.connect(url,
             // password : result.password
           });
         }
-        });
-        });
+      });
+    });
 
     /* GET  CGU. */
     router.get('/cgu', function (req, res, next) {
       res.render('cgu', { title: 'SWAPLIVE' });
     });
   });
-var connectedUsers = {} ;
-module.exports = function(users) {
-  connectedUsers = users ;
+var connectedUsers = {};
+module.exports = function (users) {
+  connectedUsers = users;
   return router;
 }
